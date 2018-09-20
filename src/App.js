31d105c969cb56226a68baf635eb46acf13d5d8b
Stephane.C <tchadizman@gmail.com>
2018-09-20 22:09:06 +0200

    Creation d'un container Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+import Header from './Container/Header'
+
 import Mission from './Component/Mission'
 import MissionData from './Component/MissionData'
 
@@ -8,12 +11,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+
+        <Header/>
+
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Site en construction
         </p>
         
         <Mission mission={new MissionData("Mission1","2018/08/03","BTL")}/>
---
+++ src/Container/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from '../Image/schapiteau.png';
+//import './Header.css'
+
+class Header extends Component {
+
+    render() {
+      return (
+        <div className="Header">
+            <header className="App-header">
+                <img src={logo} className="sitelogo" alt="logo" 
+                style={sitelogo}/>
+                <h1 className="App-title">Welcome to React</h1>          
+            </header>
+        </div>
+      );
+    }
+  }
+
+  //A exporter en CSS
+  const sitelogo = {
+    height : '80px',
+  }
+
+  export default Header;
